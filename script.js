f7b6d2e3281c856f5490dd18719d3376665537ab
JoaodeAlcantara <joao.gab0509@gmail.com>
2024-09-24 16:05:18 -0300

    marcar como concluido

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-function adicionar() {
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
     let valorInput = document.querySelector('input');
     let lista = document.createElement('li');
 
-    if(!valorInput.value){
-        document.querySelector('#erro').innerHTML = 'Vazio, adicione ulguma tarefa'
-    } else{
+    if (!valorInput.value ||  valorInput.value === ' ') {
+        document.querySelector('#erro').innerHTML = 'Vazio, adicione alguma tarefa'
+    } else {
         document.querySelector('#erro').innerHTML = ''
         lista.innerHTML = `${valorInput.value} <div id="func">
-            <span id="check" onclick="concluir(this)"><i class="fa-solid fa-square-check"></i></span>
-            <span id="trash" onclick="apagar(this)"><i class="fa-solid fa-trash-can"></i></span>
+            <span class="check" onclick="concluir(this)"><i class="fa-solid fa-square-check"></i></span>
+            <span class="trash" onclick="apagar(this)"><i class="fa-solid fa-trash-can"></i></span>
         </div>`;
 
-    document.querySelector('ul').appendChild(lista);
+        document.querySelector('ul').appendChild(lista);
 
-    valorInput.value = '';
+        valorInput.value = '';
     }
 
-    
-}
+})
 
 function apagar(elemento) {
     elemento.parentElement.parentElement.remove();
@@ -25,7 +26,16 @@ function apagar(elemento) {
 
 function concluir(elemento) {
     let item = elemento.parentElement.parentElement;
-    item.style.backgroundColor = 'rgba(13, 27, 42, 1)';
-    item.style.color = 'white';
-    item.style.textDecoration = 'line-through'
+
+    if (elemento.style.color === '') {
+        item.style.backgroundColor = 'rgba(13, 27, 42, 1)';
+        item.style.color = 'white';
+        item.style.textDecoration = 'line-through';
+        elemento.style.color = 'green';
+    } else {
+        item.style.backgroundColor = '';
+        item.style.color = '';
+        item.style.textDecoration = 'none';
+        elemento.style.color = '';
+    }
 }
