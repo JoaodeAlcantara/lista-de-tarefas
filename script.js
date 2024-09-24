7cc58a688d2a19bea4a9b2b3aadf9581f39c7dcb
JoaodeAlcantara <joao.gab0509@gmail.com>
2024-09-24 11:54:58 -0300

    style melhorado

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function adicionar() {
-    let valorInput = document.querySelector('input')
-    let lista = document.createElement('li')
+    let valorInput = document.querySelector('input');
+    let lista = document.createElement('li');
 
-    lista.innerHTML = `${valorInput.value} 
-    ${'<span onclick="apagar(this)"><img src="trash.svg" alt=""></span>'}`
+    lista.innerHTML = `${valorInput.value} <div id="func">
+            <span id="check" onclick="concluir(this)"><i class="fa-solid fa-square-check"></i></span>
+            <span id="trash" onclick="apagar(this)"><i class="fa-solid fa-trash-can"></i></span>
+        </div>`;
 
-    document.querySelector('ul').appendChild(lista)
+    document.querySelector('ul').appendChild(lista);
 
-    valorInput.value = ''
+    valorInput.value = '';
 }
 
-function apagar(lista) {
-    lista.parentElement.remove()
+function apagar(elemento) {
+    elemento.parentElement.parentElement.remove();
+}
+
+function concluir(elemento) {
+    let item = elemento.parentElement.parentElement;
+    item.style.backgroundColor = 'rgba(13, 27, 42, 1)';
+    item.style.color = 'white';
+    item.style.textDecoration = 'line-through'
 }
